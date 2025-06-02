cac71e2f47eb157bd885f192d8ef1788fd142c43
Kamlendu Shekhar <47015818+kamlendu1982@users.noreply.github.com>
2025-06-02 11:41:40 -0400

    Update my_custome_powershell.ps1

--- my_custome_powershell.ps1
+++ my_custome_powershell.ps1
@@ -1,9 +1,9 @@
 param (
     [string]$Param1,
     [string]$Param2,
-    [string]$Param3
+    [string]$Param3,
+    [string]$TriggerServer
 )
-$TriggerServer=$args[0]
 Write-Host "Hello, World"
 Write-Host "This is pulled from git and The trigger come from $TriggerServer"
 Write-Host $env:computername
